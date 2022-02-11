c726ebf1e3179c6e6dba44936b837faeb470dda2
Scott Lougheed <scott@scottlougheed.com>
2022-02-11 11:15:51 -0800

    demo storyline

--- account-mgmt/one-liners.sh
+++ account-mgmt/one-liners.sh
@@ -13,26 +13,47 @@ op item get sqlite --fields username,password
 # get UUIDs of all login items and assign to array called loginIDs
 loginIDs=($(op item list --categories Login --format=json | jq -r '.[].id'))
 
+
+op user invite --name "Nick Fury" --email "scott.lougheed+895445624@agilebits.com"
+op group user grant --group "The Avengers" --user "Nick Fury"
+
 ###
 # GROUP MANAGEMENT
 ###
 
-# list members of the IT group
-op group user list IT
+# Create a group called The Avengers 
+op group create "The Avengers" --description "The people who keep the world safe"
 
-# Make Steven Steal (VKONWSAAO5EWVEIOS2D4PRFDQQ) a manager of the IT group (ulhyycgrutra7uaozif5a7mp3m)
-op group user grant --group ulhyycgrutra7uaozif5a7mp3m --role manager --user VKONWSAAO5EWVEIOS2D4PRFDQQ
+# Create an array of UUIDs for the avengers. 
+avengers=(EFY6OJPJ3VACPNA4RUTMG45PJE NVIMFML4CFFTBFJFL6XVR6HXTU DCJGVAOEYBEPNIOPMOVRH5MOKY SDV7PHFZDBFLXB7XQOHC7UBYSU WFVQSCRPKJBMVH56PN6ZJG2WDU B2J5KCY4EBFVZLHELT5ABWFYRE)
 
-# list members of the IT group with Steven Steal included
-op group user list IT
+# loop through all avengers and add them to the group
+for avenger in $avengers; do op group user grant --group "The Avengers" --user $avenger; done 
 
-# Remove Stephen Steal from the IT Group
-op group user revoke --group ulhyycgrutra7uaozif5a7mp3m --user VKONWSAAO5EWVEIOS2D4PRFDQQ
+# invite a fearless leader to the team
+op user invite --name "Nick Fury" --email "scott.lougheed+895445624@agilebits.com"
 
+# add that leader to the Avengers group
+op group user grant --group "The Avengers" --user "Nick Fury" --role manager
+
+op group user list "The Avengers"
 
 ###
 # VAULT MANAGEMENT
 ###
+# Create some "permissions templates"
+user_permissions=view_items,view_and_copy_passwords,view_item_history
+admin_permissions=view_items,view_and_copy_passwords,view_item_history,create_items,edit_items,archive_items,delete_items,import_items,export_items,print_items
 
+# create a vault with all the super hero secrets
+op vault create "Avengers Project" --description "Info to support team of heroes who keep the world safe"
 
+# give the heroes view-related permissions
+op vault group grant --group "The Avengers" --permissions $user_permissions
 
+# give our leader some advanced permissions
+op vault user grant --user "Nick Fury" --permissions $admin_permissions
+
+# Create a document item with a proposed website banner
+cd ~/op-cli2-demo/account-mgmt
+op document create 'banner.jpg' --title "Proposed website banner" --vault "Avengers Project"
