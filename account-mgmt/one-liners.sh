d17b29378d21b27b121d40a29b0160a055c5811d
Scott Lougheed <scott@scottlougheed.com>
2022-02-14 09:18:01 -0800

    minor changes I can't remember

--- account-mgmt/one-liners.sh
+++ account-mgmt/one-liners.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-
+###
+# One Liners
+###
 # Generate lists of users in Terminal
 op user list --account michaelscottpapercompany
 
@@ -10,12 +12,9 @@ op item get Instagram
 
 op item get Instagram --fields username,password
 
-# get UUIDs of all login items and assign to array called loginIDs
-loginIDs=($(op item list --categories Login --format=json | jq -r '.[].id'))
+# get a list of item names of all login items and assign to array called loginNames
+loginNamess=($(op item list --categories Login --format=json | jq -r '.[].title'))
 
-
-op user invite --name "Nick Fury" --email "scott.lougheed+895445624@agilebits.com"
-op group user grant --group "The Avengers" --user "Nick Fury"
 
 ###
 # GROUP MANAGEMENT
@@ -27,6 +26,8 @@ op group create "The Avengers" --description "The people who keep the world safe
 # Create an array of UUIDs for the avengers. 
 avengers=(EFY6OJPJ3VACPNA4RUTMG45PJE NVIMFML4CFFTBFJFL6XVR6HXTU DCJGVAOEYBEPNIOPMOVRH5MOKY SDV7PHFZDBFLXB7XQOHC7UBYSU WFVQSCRPKJBMVH56PN6ZJG2WDU B2J5KCY4EBFVZLHELT5ABWFYRE)
 
+echo $avengers
+
 # loop through all avengers and add them to the group
 for avenger in $avengers; do op group user grant --group "The Avengers" --user $avenger; done 
 
@@ -34,7 +35,7 @@ for avenger in $avengers; do op group user grant --group "The Avengers" --user $
 op user invite --name "Nick Fury" --email "scott.lougheed+895445624@agilebits.com"
 
 # add that leader to the Avengers group
-op group user grant --group "The Avengers" --user "Nick Fury" --role manager
+op group user grant --group "The Avengers" --user "Nick Fury"
 
 op group user list "The Avengers"
 
@@ -49,10 +50,10 @@ admin_permissions=view_items,view_and_copy_passwords,view_item_history,create_it
 op vault create "Avengers Project" --description "Info to support team of heroes who keep the world safe"
 
 # give the heroes view-related permissions
-op vault group grant --group "The Avengers" --permissions $user_permissions
+op vault group grant --vault "Avengers Project" --group "The Avengers" --permissions $user_permissions
 
 # give our leader some advanced permissions
-op vault user grant --user "Nick Fury" --permissions $admin_permissions
+op vault user grant --vault "Avengers Project" --user "Nick Fury" --permissions $admin_permissions
 
 # Create a document item with a proposed website banner
 cd ~/op-cli2-demo/account-mgmt
