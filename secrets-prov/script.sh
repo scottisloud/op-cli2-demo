ad0f13b4968b021d346cb3e57db1f97bfe9fbbca
Scott Lougheed <scott@scottlougheed.com>
2022-02-27 07:44:12 -0800

    added script for secrets prpovisioning demo

---
+++ secrets-prov/script.sh
@@ -0,0 +1,9 @@
+# AWS requires two secrets for most tasks.
+# By default, and without additional planning, these secrets are stored unencrypted in a file. 
+# With `op`, plaintext credentials can be replaced with `op` secrets references 
+# You can even use variables to define multiple environments to load the correct credentials for that environment, such as $dev and $prod
+
+# this command will copy a file to an AWS S3 bucket 
+op run --env-file=.env --account bluemountainsit aws s3 cp ../account-mgmt/banner.jpg s3://enablement-demo
+
+# uploaded file available at https://enablement-demo.s3.ca-central-1.amazonaws.com/FILENAME.EXT
